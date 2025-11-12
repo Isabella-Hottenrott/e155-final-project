326f5944463d6d16cbfe489799864d30399d924d
Isabella Hottenrott <ihottenrott@g.hmc.edu>
2025-11-12 15:50:44 -0800

    added repo

--- main.c
+++ main.c
@@ -1,284 +1,84 @@
-#include "vl53l0x_api.h"
-#include "vl53l0x_platform.h"
-#include "required_version.h"
-#include "get_nucleo_serial_comm.h"
-#include <malloc.h>
+// Uncomment this line to use long range mode. This
+// increases the sensitivity of the sensor and extends its
+// potential range, but increases the likelihood of getting
+// an inaccurate reading because of reflections from objects
+// other than the intended target. It works best in dark
+// conditions.
+
+#define LONG_RANGE
 
 
-void print_pal_error(VL53L0X_Error Status){
-    char buf[VL53L0X_MAX_STRING_LENGTH];
-    VL53L0X_GetPalErrorString(Status, buf);
-    printf("API Status: %i : %s\n", Status, buf);
-}
+// Uncomment ONE of these two lines to get
+// - higher speed at the cost of lower accuracy OR
+// - higher accuracy at the cost of lower speed
 
-void print_range_status(VL53L0X_RangingMeasurementData_t* pRangingMeasurementData){
-    char buf[VL53L0X_MAX_STRING_LENGTH];
-    uint8_t RangeStatus;
-
-    /*
-     * New Range Status: data is valid when pRangingMeasurementData->RangeStatus = 0
-     */
-
-    RangeStatus = pRangingMeasurementData->RangeStatus;
-
-    VL53L0X_GetRangeStatusString(RangeStatus, buf);
-    printf("Range Status: %i : %s\n", RangeStatus, buf);
-
-}
+#define HIGH_SPEED
+//#define HIGH_ACCURACY
 
 
-VL53L0X_Error rangingTest(VL53L0X_Dev_t *pMyDevice)
-{
-    VL53L0X_Error Status = VL53L0X_ERROR_NONE;
-    VL53L0X_RangingMeasurementData_t    RangingMeasurementData;
-    int i;
-    FixPoint1616_t LimitCheckCurrent;
-    uint32_t refSpadCount;
-    uint8_t isApertureSpads;
-    uint8_t VhvSettings;
-    uint8_t PhaseCal;
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        printf ("Call of VL53L0X_StaticInit\n");
-        Status = VL53L0X_StaticInit(pMyDevice); // Device Initialization
-        print_pal_error(Status);
-    }
-    
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        printf ("Call of VL53L0X_PerformRefCalibration\n");
-        Status = VL53L0X_PerformRefCalibration(pMyDevice,
-        		&VhvSettings, &PhaseCal); // Device Initialization
-        print_pal_error(Status);
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        printf ("Call of VL53L0X_PerformRefSpadManagement\n");
-        Status = VL53L0X_PerformRefSpadManagement(pMyDevice,
-        		&refSpadCount, &isApertureSpads); // Device Initialization
-        printf ("refSpadCount = %d, isApertureSpads = %d\n", refSpadCount, isApertureSpads);
-        print_pal_error(Status);
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-
-        // no need to do this when we use VL53L0X_PerformSingleRangingMeasurement
-        printf ("Call of VL53L0X_SetDeviceMode\n");
-        Status = VL53L0X_SetDeviceMode(pMyDevice, VL53L0X_DEVICEMODE_SINGLE_RANGING); // Setup in single ranging mode
-        print_pal_error(Status);
-    }
-
-    // Enable/Disable Sigma and Signal check
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckEnable(pMyDevice,
-        		VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, 1);
-    }
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckEnable(pMyDevice,
-        		VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1);
-    }
-
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckEnable(pMyDevice,
-        		VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, 1);
-    }
-
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckValue(pMyDevice,
-        		VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD,
-        		(FixPoint1616_t)(1.5*0.023*65536));
-    }
+#include "stm32f1xx.h"
+#include "init.h"
+#include "VL53L0X.h"
+#include "usart1.h"
+#include <stdio.h>
+#include "i2c.h"
 
 
-    /*
-     *  Step  4 : Test ranging mode
-     */
+char strbuf[25]; // for UART output via sprintf()
 
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        for(i=0;i<10;i++){
-            printf ("Call of VL53L0X_PerformSingleRangingMeasurement\n");
-            Status = VL53L0X_PerformSingleRangingMeasurement(pMyDevice,
-            		&RangingMeasurementData);
+struct VL53L0X myTOFsensor = {.io_2v8 = true, .address = 0b0101001, .io_timeout = 500, .did_timeout = false};
 
-            print_pal_error(Status);
-            print_range_status(&RangingMeasurementData);
+int main(void){
+	// Initialize system timer for 1ms ticks (else divide by 1e6 for µs ticks)
+	SysTick_Config(SystemCoreClock / 1000);
+	// init the USART1 peripheral to print to serial terminal
+	init_USART1();
+	// init the I2C1 peripheral and the SDA/SCL GPIO pins
+	init_i2c1();
 
-            VL53L0X_GetLimitCheckCurrent(pMyDevice,
-            		VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, &LimitCheckCurrent);
+	USART1_transmitString("testing VL53L0X\n--------------\n");
 
-            printf("RANGE IGNORE THRESHOLD: %f\n\n", (float)LimitCheckCurrent/65536.0);
+	// GPIO pin PB5 connected to XSHUT pin of sensor
+	GPIOB->CRL &=~GPIO_CRL_CNF5;
+	GPIOB->CRL |= GPIO_CRL_MODE5;
+	GPIOB->ODR &=~GPIO_ODR_ODR5; // shut off VL53L0X
+	delay(1);
+	GPIOB->ODR |= GPIO_ODR_ODR5; // power up VL53L0X again
+	delay(2);
 
+	if( VL53L0X_init(&myTOFsensor) ){
+		USART1_transmitString("init successful\n");
+	}else{
+		USART1_transmitString("init error");
+		return 0;
+	}
 
-            if (Status != VL53L0X_ERROR_NONE) break;
-
-            printf("Measured distance: %i\n\n", RangingMeasurementData.RangeMilliMeter);
-
-
-        }
-    }
-    return Status;
-}
-
-int main(int argc, char **argv)
-{
-    VL53L0X_Error Status = VL53L0X_ERROR_NONE;
-    VL53L0X_Dev_t MyDevice;
-    VL53L0X_Dev_t *pMyDevice = &MyDevice;
-    VL53L0X_Version_t                   Version;
-    VL53L0X_Version_t                  *pVersion   = &Version;
-    VL53L0X_DeviceInfo_t                DeviceInfo;
-
-    int32_t status_int;
-    int32_t init_done = 0;
-    int NecleoComStatus = 0;
-    int NecleoAutoCom = 1;
-    TCHAR SerialCommStr[MAX_VALUE_NAME];
-
-    if (argc == 1 ) {
-	   printf("Nucleo Automatic detection selected!\n");	
-	   printf("To Specify a COM use: %s <yourCOM> \n", argv[0]);
-    } else if (argc == 2 ) {
-	   printf("Nucleo Manual COM selected!\n");
-	   strcpy(SerialCommStr,argv[1]);	   
-	   printf("You have specified %s \n", SerialCommStr);
-	   NecleoAutoCom = 0;
-    } else {
-	   printf("ERROR: wrong parameters !\n");	
-           printf("USAGE : %s <yourCOM> \n", argv[0]);
-	   return(1);
-    }
-
-    
-    
-    printf ("VL53L0X API Simple Ranging example\n\n");
-//    printf ("Press a Key to continue!\n\n");
-//    getchar();
-
-    if (NecleoAutoCom == 1) {
-	    // Get the number of the COM attached to the Nucleo
-	    // Note that the following function will look for the 
-	    // Nucleo with name USBSER000 so be careful if you have 2 Nucleo 
-	    // inserted
-
-	    printf("Detect Nucleo Board ...");
-	    NecleoComStatus = GetNucleoSerialComm(SerialCommStr);
-	    
-	    if ((NecleoComStatus == 0) || (strcmp(SerialCommStr, "") == 0)) {
-		    printf("Error Nucleo Board not Detected!\n");
-		    return(1);
-	    }
-	    
-	    printf("Nucleo Board detected on %s\n\n", SerialCommStr);
-    }
-    
-    // Initialize Comms
-    pMyDevice->I2cDevAddr      = 0x52;
-    pMyDevice->comms_type      =  1;
-    pMyDevice->comms_speed_khz =  400;
-    
-    //Just inserted this for us.
-    void init_i2c1();
-
-
-   Status = VL53L0X_i2c_init(SerialCommStr, 460800);
-    if (Status != VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_ERROR_CONTROL_INTERFACE;
-        init_done = 1;
-    } else
-        printf ("Init Comms\n");
-
-    /*
-     * Disable VL53L0X API logging if you want to run at full speed
-     */
-#ifdef VL53L0X_LOG_ENABLE
-    VL53L0X_trace_config("test.log", TRACE_MODULE_ALL, TRACE_LEVEL_ALL, TRACE_FUNCTION_ALL);
+#ifdef LONG_RANGE
+	// lower the return signal rate limit (default is 0.25 MCPS)
+	VL53L0X_setSignalRateLimit(&myTOFsensor, 0.1);
+	// increase laser pulse periods (defaults are 14 and 10 PCLKs)
+	VL53L0X_setVcselPulsePeriod(&myTOFsensor, VcselPeriodPreRange, 18);
+	VL53L0X_setVcselPulsePeriod(&myTOFsensor, VcselPeriodFinalRange, 14);
+#endif
+#ifdef HIGH_SPEED
+	// reduce timing budget to 20 ms (default is about 33 ms)
+	VL53L0X_setMeasurementTimingBudget(&myTOFsensor, 20000);
+	USART1_transmitString("step1\n");
+#else //HIGH_ACCURACY
+	// increase timing budget to 200 ms
+	VL53L0X_setMeasurementTimingBudget(&myTOFsensor, 200000);
 #endif
 
+	VL53L0X_startContinuous(&myTOFsensor, 0);
 
-    /*
-     *  Get the version of the VL53L0X API running in the firmware
-     */
+	while(1){
+		uint16_t value = VL53L0X_readRangeContinuousMillimeters(&myTOFsensor);
+		sprintf(strbuf, "\t%d\tmm\n", value);
+		USART1_transmitString(strbuf);
+		 if ( VL53L0X_timeoutOccurred(&myTOFsensor) ) {
+			 USART1_transmitString("TIMEOUT\n");
+		 }
+	}
 
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        status_int = VL53L0X_GetVersion(pVersion);
-        if (status_int != 0)
-            Status = VL53L0X_ERROR_CONTROL_INTERFACE;
-    }
-
-    /*
-     *  Verify the version of the VL53L0X API running in the firmware
-     */
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        if( pVersion->major != VERSION_REQUIRED_MAJOR ||
-            pVersion->minor != VERSION_REQUIRED_MINOR ||
-            pVersion->build != VERSION_REQUIRED_BUILD )
-        {
-            printf("VL53L0X API Version Error: Your firmware has %d.%d.%d (revision %d). This example requires %d.%d.%d.\n",
-                pVersion->major, pVersion->minor, pVersion->build, pVersion->revision,
-                VERSION_REQUIRED_MAJOR, VERSION_REQUIRED_MINOR, VERSION_REQUIRED_BUILD);
-        }
-    }
-
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        printf ("Call of VL53L0X_DataInit\n");
-        Status = VL53L0X_DataInit(&MyDevice); // Data initialization
-        print_pal_error(Status);
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        Status = VL53L0X_GetDeviceInfo(&MyDevice, &DeviceInfo);
-        if(Status == VL53L0X_ERROR_NONE)
-        {
-            printf("VL53L0X_GetDeviceInfo:\n");
-            printf("Device Name : %s\n", DeviceInfo.Name);
-            printf("Device Type : %s\n", DeviceInfo.Type);
-            printf("Device ID : %s\n", DeviceInfo.ProductId);
-            printf("ProductRevisionMajor : %d\n", DeviceInfo.ProductRevisionMajor);
-        printf("ProductRevisionMinor : %d\n", DeviceInfo.ProductRevisionMinor);
-
-        if ((DeviceInfo.ProductRevisionMinor != 1) && (DeviceInfo.ProductRevisionMinor != 1)) {
-        	printf("Error expected cut 1.1 but found cut %d.%d\n",
-                       DeviceInfo.ProductRevisionMajor, DeviceInfo.ProductRevisionMinor);
-                Status = VL53L0X_ERROR_NOT_SUPPORTED;
-            }
-        }
-        print_pal_error(Status);
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        Status = rangingTest(pMyDevice);
-    }
-
-    print_pal_error(Status);
-    
-    // Implementation specific
-
-    /*
-     *  Disconnect comms - part of VL53L0X_platform.c
-     */
-
-    if(init_done == 0)
-    {
-        printf ("Close Comms\n");
-        status_int = VL53L0X_comms_close();
-        if (status_int != 0)
-            Status = VL53L0X_ERROR_CONTROL_INTERFACE;
-    }
-
-    print_pal_error(Status);
-    return (0);
+return 1;
 }
-
-
-
