c8553905959ad2fadeecb1113ce21ab8f2f6bf1b
Isabella Hottenrott <ihottenrott@g.hmc.edu>
2025-12-04 05:26:20 -0800

    working .sv

--- fpga/chooseaddr.sv
+++ fpga/chooseaddr.sv
@@ -4,9 +4,9 @@ module chooseaddr(input logic clk,
                 input logic [5:0] directedBits,
                 input logic [1:0] thiscomputer,
                 input logic [1:0] result,
-                output logic [5:0] screencode);
+                output logic [3:0] screencode);
 
-logic [6:0] screencodeintermed;
+logic [3:0] screencodeintermed, bitswizzlecomputer, bitswizzleresult;
 
 #define rstScreen 00000111
 #define startScreen 00001011
@@ -17,29 +17,32 @@ logic [6:0] screencodeintermed;
 #define tenDoneScreen 01000111
 
 
+assign bitswizzlecomputer = {2'b00, thiscomputer};
+assign bitswizzleresult = {2'b00, result};
+
 always_comb begin
     case(directedBits) // can change all these values once addresses are agreed on
-        6'b000001: screencodeintermed = directedBits; // reset
-        6'b000010: screencodeintermed = directedBits; // start
-        6'b000011: screencodeintermed = directedBits; // place your move
-        6'b010000: screencodeintermed = directedBits; // play next round
-        6'b010001: screencodeintermed = directedBits; // 10 rounds done
-        6'b100100: screencodeintermed = directedBits + thiscomputer; // will take up
-                                                              // 7'b0000101 (computer rock)
-                                                              // 7'b0000110 (computer paper)        
-                                                              // 7'b0000111 (computer scissors)
-        6'b101000: screencodeintermed = directedBits + result;       // will take up
-                                                              // 7'b1001001 (computer win)
-                                                              // 7'b1001010 (computer lose)        
-                                                              // 7'b1001011 (computer draw)
-        default:    screencodeintermed = 6'b000000;
+        6'b000001: screencodeintermed = 4'b0000; // reset
+        6'b000010: screencodeintermed = 4'b0001; // start
+        6'b000011: screencodeintermed = 4'b0010; // place your move
+        6'b010000: screencodeintermed = 4'b0011; // play next round
+        6'b010001: screencodeintermed = 4'b0100; // 10 rounds done
+        6'b100100: screencodeintermed = 4'b0101 + bitswizzlecomputer; // will take up
+                                                              // 4'b0101 (computer rock)
+                                                              // 4'b0110 (computer paper)        
+                                                              // 4'b0111 (computer scissors)
+        6'b101000: screencodeintermed = 4'b0111 + bitswizzleresult;       // will take up
+                                                              // 4'b0111 (computer win)
+                                                              // 4'b1000 (computer lose)        
+                                                              // 4'b1001 (computer draw)
+        default:    screencodeintermed = 4'b0000;
     endcase
 end
 
 
 always @(posedge clk) begin
 if (reset)
-screencode <= 6'b0;
+screencode <= 4'b0;
 else if (screenWE)
 screencode <= screencodeintermed;
 else
--- fpga/finalproj.sv
+++ fpga/finalproj.sv
@@ -12,7 +12,7 @@ module finalproj(input logic sck,
 logic done, interpretWE, screenWE, clk;
 logic [7:0] unsynchSPIbyte, synchSPIbyte;
 logic [6:0] directedBits;
-logic [5:0] screencode;
+logic [3:0] screencode;
 logic [1:0] thiscomputer, result,
 
 
